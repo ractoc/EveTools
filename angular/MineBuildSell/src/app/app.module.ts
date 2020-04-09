27612ebf17e4b3b08824970ffee0843b93395633
Mark Schrijver <ractoc@ractoc.com>
2020-04-09 23:28:04 +0200

    added a search field to the list of blueprints. This helps narrow down the displayed blueprints to make it easier to find what you are looking for.

--- angular/MineBuildSell/src/app/app.module.ts
+++ angular/MineBuildSell/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
@@ -27,7 +27,8 @@ import {BlueprintDetailsComponent} from './blueprint-details/blueprint-details.c
     HttpClientModule,
     ReactiveFormsModule,
     RouterModule,
-    RouterModule.forRoot(APP_ROUTES)
+    RouterModule.forRoot(APP_ROUTES),
+    FormsModule
   ],
   providers: [],
   bootstrap: [MainComponent]
--- angular/MineBuildSell/src/app/blueprint-list/blueprint-list.component.ts
+++ angular/MineBuildSell/src/app/blueprint-list/blueprint-list.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {BlueprintModel} from '../shared/model/blueprint.model';
 import {AssetsService} from '../service/assets.service';
-import {ActivatedRoute, Router} from "@angular/router";
-import {Subscription} from "rxjs";
+import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-blueprint-list',
@@ -12,9 +12,11 @@ import {Subscription} from "rxjs";
 export class BlueprintListComponent implements OnInit, OnDestroy {
 
   blueprints: BlueprintModel[];
+  displayBlueprints: BlueprintModel[];
   errorMessage: string;
   bpType: string;
   private routeListener$: Subscription;
+  search: string;
 
   constructor(
     private route: ActivatedRoute, private assetsService: AssetsService, private router: Router) {
@@ -27,10 +29,14 @@ export class BlueprintListComponent implements OnInit, OnDestroy {
           if (data.personal) {
             this.bpType = 'personal';
             this.assetsService.getPersonalBlueprints().subscribe(
-              (blueprintData: BlueprintModel[]) =>
-                blueprintData && blueprintData.length > 0
-                  ? this.blueprints = blueprintData
-                  : this.errorMessage = 'No blueprints to display',
+              (blueprintData: BlueprintModel[]) => {
+                if (blueprintData && blueprintData.length > 0) {
+                  this.blueprints = blueprintData;
+                  this.displayBlueprints = blueprintData;
+                } else {
+                  this.errorMessage = 'No blueprints to display';
+                }
+              },
               err => {
                 this.errorMessage = err;
                 if (err.status === 401) {
@@ -42,10 +48,14 @@ export class BlueprintListComponent implements OnInit, OnDestroy {
           } else if (data.corporate) {
             this.bpType = 'corporate';
             this.assetsService.getCorporateBlueprints().subscribe(
-              (blueprintData: BlueprintModel[]) =>
-                blueprintData && blueprintData.length > 0
-                  ? this.blueprints = blueprintData
-                  : this.errorMessage = 'No blueprints to display',
+              (blueprintData: BlueprintModel[]) => {
+                if (blueprintData && blueprintData.length > 0) {
+                  this.blueprints = blueprintData;
+                  this.displayBlueprints = blueprintData;
+                } else {
+                  this.errorMessage = 'No blueprints to display';
+                }
+              },
               err => {
                 this.errorMessage = err;
                 if (err.status === 401) {
@@ -68,5 +78,9 @@ export class BlueprintListComponent implements OnInit, OnDestroy {
       'list-group-item': true,
       'list-group-item-action': true,
     };
+  }
+
+  searchBp() {
+    this.displayBlueprints = this.blueprints.filter(bp => bp.name.toLowerCase().includes(this.search.toLowerCase()));
   }
 }
