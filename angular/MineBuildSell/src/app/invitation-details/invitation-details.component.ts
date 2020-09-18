192647bdd2955858f72d2556e19a528256c5c3f8
Mark Schrijver <ractoc@ractoc.com>
2020-09-18 16:22:37 +0200

    added corporation level invitations to the fleet manager. When you create a fleet event, and set it to corporation restricted, this will automatically send an ingame-mail to that corporation. This mail contains a link to a page where people can then see the details of the fleet event. Eventually, it will be possible for people to use this detail page to either register or decline for the fleet invite.

---
+++ angular/MineBuildSell/src/app/invitation-details/invitation-details.component.ts
@@ -0,0 +1,83 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {ActivatedRoute, Router} from '@angular/router';
+import {LocalStorageService} from '../service/local-storage.service';
+import {InviteModel} from '../shared/model/invite-model';
+import {InviteService} from '../service/invite.service';
+import {User} from '../shared/model/user.model';
+import {UserService} from '../service/user.service';
+
+@Component({
+  selector: 'app-invitation-details',
+  templateUrl: './invitation-details.component.html',
+  styleUrls: ['./invitation-details.component.css']
+})
+export class InvitationDetailsComponent implements OnInit, OnDestroy {
+  private routeListener$: Subscription;
+
+  public errorMessage: string;
+  public invite: InviteModel;
+  public user: User;
+  public start: any;
+
+  constructor(
+    private inviteService: InviteService,
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private localStorageService: LocalStorageService) {
+  }
+
+  ngOnInit(): void {
+    this.errorMessage = undefined;
+    this.routeListener$ = this.route.params
+      .subscribe((params: any) => {
+          if (params.key) {
+            this.inviteService.getInvite(params.key).subscribe(
+              (inviteData: InviteModel) => {
+                this.invite = inviteData;
+                this.start = JSON.parse(inviteData.fleet.start)
+                this.user = this.userService.getCurrentUser();
+              },
+              err => {
+                console.log('error opening invite details')
+                if (err.status === 401) {
+                  console.log('redirecting to login');
+                  this.localStorageService.set('currentPage', '/fleets/invite/' + params.key);
+                  this.router.navigateByUrl('/login');
+                } else {
+                  this.errorMessage = 'Unable to load invite';
+                }
+              }
+            );
+          }
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.routeListener$.unsubscribe();
+  }
+
+  displayDate() {
+    return this.prefixZero(this.start.date.day)
+      + '-'
+      + this.prefixZero(this.start.date.month)
+      + '-'
+      + this.prefixZero(this.start.date.year);
+  }
+
+  displayTime() {
+    return this.prefixZero(this.start.time.hour)
+      + ':'
+      + this.prefixZero(this.start.time.minute)
+  }
+
+  private prefixZero(digit: number) {
+    if (digit < 10) {
+      return '0' + digit;
+    } else {
+      return '' + digit
+    }
+  }
+}
---
+++ angular/MineBuildSell/src/app/service/invite.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {UserService} from './user.service';
+import {environment} from '../../environments/environment';
+import {map} from 'rxjs/operators';
+
+const INVITES_URI = 'http://' + environment.apiHost + ':8282/fleets/invites';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InviteService {
+
+  constructor(private http: HttpClient, private userService: UserService) {
+  }
+
+  getInvite(key: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + this.userService.getEveState()
+      })
+    };
+    return this.http.get<any>(INVITES_URI + '/' + key, httpOptions)
+      .pipe(
+        map(result => {
+          if (result.responseCode >= 400) {
+            throw new Error('broken API:' + result.responseCode);
+          } else {
+            return result.invite;
+          }
+        })
+      );
+  }
+}
