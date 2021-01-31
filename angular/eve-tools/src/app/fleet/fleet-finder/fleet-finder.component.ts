e3faaeef8b717fdedd0644fa63f0d6eb42bbf534
Mark Schrijver <ractoc@ractoc.com>
2021-01-31 01:47:04 +0100

    material theme fix code restructuring fleet finder added, currently implemented in Angular: date range, types

--- angular/eve-tools/src/app/fleet/fleet-finder/fleet-finder.component.ts
+++ angular/eve-tools/src/app/fleet/fleet-finder/fleet-finder.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {TypeService} from "../../services/type.service";
+import {Type} from "../../services/model/type";
+import {UserService} from "../../services/user.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {LocalStorageService} from "../../../../../MineBuildSell/src/app/service/local-storage.service";
 
 @Component({
   selector: 'app-fleet-finder',
@@ -7,9 +13,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FleetFinderComponent implements OnInit {
 
-  constructor() { }
+  fleetForm = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+    types: new FormControl(),
+  });
+
+  private types: Type[];
+
+  constructor(private router: Router,
+              private userService: UserService,
+              private typeService: TypeService,
+              private localStorageService: LocalStorageService) {
+  }
 
   ngOnInit(): void {
+    if (!this.userService.getCurrentUser()) {
+      this.localStorageService.set('currentPage', '/fleet/find');
+      this.router.navigateByUrl('/user/login');
+    }
+    this.loadTypes();
+  }
+
+  private loadTypes() {
+    this.typeService.loadTypes().subscribe(
+      (typeData: Type[]) => {
+        console.log('setting types', typeData);
+        this.types = typeData;
+      }
+    );
   }
 
 }
